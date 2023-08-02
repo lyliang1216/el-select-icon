6b72d7040c805a866ef9c0e78bc261875fd888ea
刘银亮 <lyliang@liuyinliangdeMacBook-Pro.local>
2023-08-02 15:25:12 +0800

    feat: 修改依赖注册方式

--- packages/selectIcon/index.ts
+++ packages/selectIcon/index.ts
@@ -1,9 +1,16 @@
 import type { App } from "vue";
 import SelectIcon from "./SelectIcon.vue";
 
+import ElementPlus from "element-plus";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+
 // 使用install方法，在app.use挂载
 SelectIcon.install = (app: App) => {
   app.component(SelectIcon.__name as string, SelectIcon);
+  for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component);
+  }
+  (app as any).use(ElementPlus);
 };
 
 export default SelectIcon;
--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,7 @@ import "./style.css";
 import App from "./App.vue";
 import "element-plus/dist/index.css";
 import "element-plus/theme-chalk/dark/css-vars.css";
-import ElementPlus from "element-plus";
-import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
 const app: any = createApp(App);
-
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component);
-}
-app.use(ElementPlus);
 
 app.mount("#app");

83c8813bea5da7bd3c0f13472b4a62d270b19aca
刘银亮 <lyliang@liuyinliangdeMacBook-Pro.local>
2023-08-03 10:52:20 +0800

    refactor: 修改组件注册位置

--- packages/index.ts
+++ packages/index.ts
@@ -1,10 +1,18 @@
-import type { App } from "vue";
+import { createApp, type App } from "vue";
 import SelectIcon from "./selectIcon";
+
+import ElementPlus from "element-plus";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+
+const _app = createApp(SelectIcon);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  _app.component(key, component);
+}
+(_app as any).use(ElementPlus);
 
 const components = [SelectIcon];
 
 const install = (app: App): void => {
-  console.log(app, "app2");
   // 遍历注册所有组件
   /*
     component.__name ts报错
